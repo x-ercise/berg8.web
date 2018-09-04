dc033713647c40b1e7d7903d5def6b8f0f6686a1
Danai.T <danai.thit@gmail.com>
2018-09-04 22:25:21 +0700

    Comment

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import reducers from "./../reducers";
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { helloSaga } from '../sagas'
+//import { helloSaga } from '../sagas'
 
 const sagaMiddleware = createSagaMiddleware()
 const stores = createStore(reducers, 
