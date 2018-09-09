5fa0b8b2f36c7fd166aac8e769ae31a1e25e296f
Danai.T <danai.t@DanaiTs-MacBook-Pro.local>
2018-09-09 19:54:55 +0700

    Remove Transformdata on Reducer

--- src/reducers/waitingListPageReducer.js
+++ src/reducers/waitingListPageReducer.js
@@ -31,34 +31,26 @@ const defaultState = {
     myTasks : [],
 };
 
-const TransfromData = (data) => {
-    let newList = data.map((e) => ({ key: e.documentNo, ...e }));
-    return newList;
-};
-
 const waitingListPageReducer = (state = defaultState, action) => {
     switch (action.type) {
         case INI_FILTER_WAITING_PAGE: {
             return { ...state, filter: { ...action.payload } };
         }
         case INIT_DATA_WAITING_PAGE:
-            return { ...state, data: TransfromData(action.data) };
+            return { ...state, data: action.data };
         case SET_SELECTED_LIST_WAITING_PAGE:
-
-            return Object.assign({}, state, {
-                selected: action.selected
-            });
+            return {...state, ...{ selected: action.selected}};
         case ON_FILTER_WAITING_PAGE:
             return { ...state, ...{ filter: { ...action.payload }, selected: [] } };
         case ON_CRITERIA_CHANGE_WAITING_PAGE:
             return { ...state, filter: { ...action.payload } };
         case ON_SET_DATE_TABLE_WAITING_PAGE:
-            return { ...state, data: TransfromData(action.data) };
+            return { ...state, data: action.data };
         case ON_CLICK_BUTTON_WAITING_PAGE:
             return { ...state, filter: { ...action.payload } };
         case ON_ACTION_RESPONSE_WAITING_PAGE:
             if (action.clearSelected) {
-                return { ...state, ...{ data: TransfromData(action.data), selected: [] } };
+                return { ...state, ...{ data: action.data, selected: [] } };
             }
             return state;
         case API_REQUEST_COMMAND_WAITING_PAGE:
