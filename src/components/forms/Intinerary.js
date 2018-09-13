ff4e1d39dd1c317df3003d34d2aedf1ff879fea1
x-ercise <x.fronzen@gmail.com>
2018-09-13 20:15:56 +0700

    sample style

--- src/components/forms/Intinerary.js
+++ src/components/forms/Intinerary.js
@@ -5,12 +5,11 @@ import { Card, Button, Form, Col, Row } from 'antd';
 const Intinerary = (props) => {
     return (<Card title="Intinerary">
         <Form onSubmit={props.handleSubmitIntinerary}>
-
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
             <Form.Item><Button htmlType="submit">SAVE</Button></Form.Item>
         </Form>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
     </Card>)
 }
 
